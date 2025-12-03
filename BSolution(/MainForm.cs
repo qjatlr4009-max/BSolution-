cacffdcc36834e25ced746ce18f968c8a997182a
user <user@DESKTOP-AI5IPQK>
2025-12-03 13:54:29 +0900

    초기 커밋.

---
+++ BSolution(/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace BSolution_
+{
+    public partial class MainForm : Form
+    {
+        private static DockPanel _dockPanel;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _dockPanel = new DockPanel
+            {
+                Dock = DockStyle.Fill
+            };
+            Controls.Add(_dockPanel);
+
+
+            _dockPanel.Theme = new VS2015DarkTheme();
+           
+            LoadDockingWindows();
+        }
+        
+        private void LoadDockingWindows()
+
+        {
+            CameraForm cameraForm = new CameraForm();
+            cameraForm.Show(_dockPanel, DockState.Document);
+
+            ResultForm resultForm = new ResultForm();
+            resultForm.Show(cameraForm.Pane, DockAlignment.Bottom, 0.3);
+
+            var propForm = new PropertiesForm();
+            propForm.Show(_dockPanel, DockState.DockRight);
+
+            var StatisticForm = new StatisticForm();
+            StatisticForm.Show(_dockPanel, DockState.DockRight);
+
+            var LogForm = new LogForm();
+            LogForm.Show(propForm.Pane, DockAlignment.Bottom, 0.3);
+
+
+        }
+    }
+}
